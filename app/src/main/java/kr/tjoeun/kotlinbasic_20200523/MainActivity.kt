d5c1a500f0e08c693c2010593ba0b27d7f0fd8d3
janggalman <janggalman@gmail.com>
2020-05-23 16:19:58 +0900

    버튼 누르면 => 텍스트뷰 내용 (text) 변경

--- app/src/main/java/kr/tjoeun/kotlinbasic_20200523/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/kotlinbasic_20200523/MainActivity.kt
@@ -12,8 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         
         okBtn.setOnClickListener { 
-            
-            Log.d("메인화면", "확인버튼눌림")
+
+//            로그로 버튼 이벤트 확인
+//            Log.d("메인화면", "확인버튼눌림")
+
+            resultTxt.text = "버튼이 눌림"
+
         }
         
         
